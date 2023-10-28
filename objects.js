e402dd7e5524d5d41aa05e2f92c1dfe1bd23d2ad
Rudra Kumar Mishra <137492251+rudrakumarmishraa@users.noreply.github.com>
2023-10-28 12:39:28 +0530

    Exported an object

---
+++ objects.js
@@ -0,0 +1,15 @@
+Object = {
+    one :{
+    name: "Rudra Kumar Mishra",
+        favNum : 10,
+            developer : true,
+    },
+    two : {
+    name: "Rudra Kumar Mishra",
+    favNum : 10,
+    developer : true,
+    }
+}
+
+// Exporting module
+module.exports = Object;
